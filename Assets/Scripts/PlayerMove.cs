87f30f0b3f29abbb650cc7304b96ac2af1477afb
zion <jd060625@naver.com>
2024-01-21 08:10:39 +0900

    player anim

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,23 +4,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    [SerializeField]private float speed;
-    [SerializeField] private float jumpPower;
+    [Header("Move")]
+    [SerializeField]private int speed;
     private float axis;
+    
+    [Header("Jump")]
+    [SerializeField] private float jumpPower;
+
+    private Animator anim;
     private Rigidbody2D rigid;
+    private SpriteRenderer spr;
+    private static readonly int isJumping = Animator.StringToHash("isJumping");
+
     void Start() {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
         axis = Input.GetAxisRaw("Horizontal");
+        anim.SetInteger("speed", (int)axis);
+        spr.flipX = axis < 0;
         
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && !anim.GetBool(isJumping)) {
             rigid.velocity = new Vector2(rigid.velocity.x, jumpPower);
+            anim.SetBool(isJumping, true);
         }
     }
 
     private void FixedUpdate() {
         rigid.velocity = new Vector2(axis * speed, rigid.velocity.y);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ContactPoint2D contact = other.contacts[0];
+        Vector2 normal = contact.normal;
+        if (other.gameObject.CompareTag("Ground") && Mathf.Abs(normal.x) < normal.y)
+        {
+            anim.SetBool(isJumping, false);
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        }
     }
 }
